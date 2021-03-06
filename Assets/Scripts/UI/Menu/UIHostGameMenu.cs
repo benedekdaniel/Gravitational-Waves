ac41f1a2e964d1142fba42e17e92a14b2d44f2de
Soma Somorjai <somorjais@cardiff.ac.uk>
2021-03-06 19:06:54 +0100

    Fix UI sizes to Main Camera size. Remove main menu background fade and localization picker.

--- Assets/Scripts/UI/Menu/UIHostGameMenu.cs
+++ Assets/Scripts/UI/Menu/UIHostGameMenu.cs
@@ -2,17 +2,39 @@ using Game.Managers;
 using Mirror;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIHostGameMenu : MonoBehaviour
 {
+
+    public TMP_InputField GameCodeUIElement;
+
+    private NewNetworkDiscovery newNetworkDiscovery;
+
+
+    private void Awake()
+    {
+        newNetworkDiscovery = FindObjectOfType<NewNetworkDiscovery>();
+
+        SetGameCodeOnUI(newNetworkDiscovery.GameCode);
+    }
+
+    
+
     public void HostGame()
     {
-        var newNetworkDiscovery = GameObject.FindObjectOfType<NewNetworkDiscovery>();
-        MySceneManager.LoadScene("GameView");
+        SceneManager.LoadScene("GameView", LoadSceneMode.Single);
         NetworkManager.singleton.StartHost();
         newNetworkDiscovery.AdvertiseServer();
+    }
 
+    private void SetGameCodeOnUI(string gameCode)
+    {
+        if (GameCodeUIElement == null) 
+            throw new MissingReferenceException("Cannot set game code on the UI. No Game Code UI Element provided.");
+
+        GameCodeUIElement.text = gameCode;
     }
 }
